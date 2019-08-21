02fc323807ba58227606a83bc2eec511eaf03b31
Callin Switzer <callin.switzer@gmail.com>
2019-08-21 16:09:38 -0700

    tested with 6 sensors

--- ArduinoCode/PhotoGateReader/PhotoGateReader.ino
+++ ArduinoCode/PhotoGateReader/PhotoGateReader.ino
@@ -1,5 +1,5 @@
 // set the number of analog pins you want to use
-int numReadings = 2;
+int numReadings = 6;
 
 
 // select the input pins
@@ -9,15 +9,12 @@ int sensorPins[6] = {A0, A1, A2, A3, A4, A5};
 int analogData[6];
 
 void setup() {
-
   Serial.begin(9600); 
 }
 
 void loop() {
   // read the value from the sensor:
   for (int i = 0; i < numReadings; i++) {
-    //analogRead(sensorPins[i]);
-    //delay(10); // delay for muxer
     analogData[i] =  analogRead(sensorPins[i]);
     Serial.print(analogData[i]);
     // add comma 
@@ -26,4 +23,5 @@ void loop() {
     }
   }
   Serial.print("\n");
+  Serial.flush();
 }
