9841cf72e56fb8baf50fd4cbc31d89c3c91064f6
Callin Switzer <callin.switzer@gmail.com>
2019-09-03 15:38:48 -0700

    updated code for mac

--- ArduinoCode/PhotoGateReader/PhotoGateReader.ino
+++ ArduinoCode/PhotoGateReader/PhotoGateReader.ino
@@ -8,20 +8,26 @@ int sensorPins[6] = {A0, A1, A2, A3, A4, A5};
 // variables for analog reads
 int analogData[6];
 
+int val;
+
 void setup() {
   Serial.begin(19200); 
+  Serial.println(" Setup complete");
 }
 
 void loop() {
-  // read the value from the sensor:
-  for (int i = 0; i < numReadings; i++) {
-    analogData[i] =  analogRead(sensorPins[i]);
-    Serial.print(analogData[i]);
-    // add comma 
-    if (i < (numReadings-1)) {
-         Serial.print(",");
-    }
+  val = Serial.read();
+  if (val == 114) { // 114 == "r"
+      // read the value from the sensor:
+      for (int i = 0; i < numReadings; i++) {
+        analogData[i] =  analogRead(sensorPins[i]);
+        Serial.print(analogData[i]);
+        // add comma 
+        if (i < (numReadings-1)) {
+             Serial.print(",");
+        }
+      }
+      Serial.print("\n");
+      //Serial.flush();
   }
-  Serial.print("\n");
-  Serial.flush();
 }
