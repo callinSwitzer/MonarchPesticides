8c53ca56e5a54b4f1d521f71ec29e232b877e40d
Callin Switzer <callin.switzer@gmail.com>
2019-09-05 16:01:09 -0700

    tried to fix sampling rate

--- ArduinoCode/PhotoGateReader/PhotoGateReader.ino
+++ ArduinoCode/PhotoGateReader/PhotoGateReader.ino
@@ -1,3 +1,4 @@
+
 // set the number of analog pins you want to use
 int numReadings = 6;
 
@@ -5,17 +6,22 @@ int numReadings = 6;
 // select the input pins
 int sensorPins[6] = {A0, A1, A2, A3, A4, A5};
 
+long current_time = 0L;
+long previous_time = 0;
+
 // variables for analog reads
 int analogData[6];
 
 int val;
 
 void setup() {
-  Serial.begin(19200); 
+  Serial.begin(115200); 
   Serial.println(" Setup complete");
 }
 
 void loop() {
+
+  current_time = micros();
   val = Serial.read();
   if (val == 114) { // 114 == "r"
       // read the value from the sensor:
@@ -27,7 +33,11 @@ void loop() {
              Serial.print(",");
         }
       }
+      
+      Serial.print(",");
+      Serial.print(current_time - previous_time);
       Serial.print("\n");
-      //Serial.flush();
+      previous_time = current_time; 
+      
   }
 }
